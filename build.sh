6e3340aea79f0d7f0b0ad20591cfd72821b09815
peter279k <peter279k@gmail.com>
2021-07-17 13:51:26 +0800

    Add video_info.php to override and format EOF

--- build.sh
+++ build.sh
@@ -16,5 +16,11 @@ rm -rf ./.git
 
 php ../composer.phar install -n --no-dev
 
+dos2unix *.html
+dos2unix *.php
+
 cp ../index.html ./public/
 cp ../download.php ./public/
+cp ../video_info.php ./public/
+
+echo "Building Web YouTube Downloader has been done."
